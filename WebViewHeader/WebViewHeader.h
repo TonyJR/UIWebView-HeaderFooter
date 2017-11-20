01968637b0f9335f2a8c738a378b7dbc717d17ef
hai@leftcoastlogic.com <hai@leftcoastlogic.com>
2017-11-20 17:19:15 +0700

    - Support Carthage

---
+++ WebViewHeader/WebViewHeader.h
@@ -0,0 +1,19 @@
+//
+//  WebViewHeader.h
+//  WebViewHeader
+//
+//  Created by Nguyen Thanh Hai on 11/20/17.
+//  Copyright © 2017 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebViewHeader.
+FOUNDATION_EXPORT double WebViewHeaderVersionNumber;
+
+//! Project version string for WebViewHeader.
+FOUNDATION_EXPORT const unsigned char WebViewHeaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebViewHeader/PublicHeader.h>
+
+
